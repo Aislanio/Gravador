3de41fcb85443c55a5fc2bd45e5af70c67337558
Aislanio <aislanioamorim@gmail.com>
2021-04-29 18:55:27 -0300

    second commit

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ recognizer.onresult = function(event){
                 transcription.textContent = "";
                 for (var i = event.resultIndex; i < event.results.length; i++) {
                     if(event.results[i].isFinal){
-                        transcription.textContent = event.results[i][0].transcript+' (Taxa de acerto [0/1] : ' + event.results[i][0].confidence + ')';
+                        transcription.textContent += event.results[i][0].transcript;
                     }else{
                         transcription.textContent += event.results[i][0].transcript;
                     }
